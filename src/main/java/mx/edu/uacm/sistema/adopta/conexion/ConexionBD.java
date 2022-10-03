04cbc2c0d8d7f68a0bdd2ecc55242197c394ffa6
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2022-10-02 19:44:27 -0500

    Creación del Proyecto en Mariadb como manejador de Base de datos

--- src/main/java/mx/edu/uacm/sistema/adopta/conexion/ConexionBD.java
+++ src/main/java/mx/edu/uacm/sistema/adopta/conexion/ConexionBD.java
@@ -1,4 +1,19 @@
 package mx.edu.uacm.sistema.adopta.conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+    private static String url = "jdbc:mysql://localhost/bdadopta";
+    private static String username = "root";
+    private static String password = "";
+    private static Connection connection;
+
+    public static Connection getInstance() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(url, username, password);
+        }
+        return connection;
+    }
 }
